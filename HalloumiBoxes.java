fa850e23788751fdbc42a0129e83ca5f7e7cd090
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-02 22:01:03 +0530

    1901A and 1903A

---
+++ HalloumiBoxes.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class HalloumiBoxes {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();  //read number of test cases
+        while(t-- >0){
+            long n = sc.nextLong(); // Read the number of boxes
+			long k = sc.nextLong(); // Read the maximum length of subarray that can be reversed
+			long[] a = new long[(int) n]; // Array to store the numbers on the boxes
+            for (int i = 0; i < n; i++) {
+				a[i] = sc.nextLong(); // Read each number into the array
+			}
+            long[] copy_a = Arrays.copyOf(a, a.length); // Create a copy of the original array
+            Arrays.sort(copy_a); // Sort the copy of the array
+           if (Arrays.equals(copy_a, a) || k > 1) {
+				System.out.println("YES"); // If true, print "YES"
+			} else {
+				System.out.println("NO"); // Otherwise, print "NO"
+			}
+        }
+    }
+}
---
+++ LineTrip.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+
+public class LineTrip {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt(); // Read the number of test cases
+        while (t-- > 0) {
+            long n = sc.nextLong(); //no. of gas stations
+            long x = sc.nextLong();
+
+            //List to store points including start ,gas stations and end point
+            List<Long> pointsList = new ArrayList<>();
+            pointsList.add(0L); // Add starting point
+            for (int i = 0; i < n; i++) {
+                long point = sc.nextLong();
+                pointsList.add(point); // Add gas station points
+            }
+            pointsList.add(x); // Add end point
+            n = pointsList.size(); // Update n to include start and end points
+
+            long maxDistanceBetweenPoints = 0; // Variable to track the maximum distance between consecutive points
+            for (int i = 1; i < n; i++) {
+                if (i == n - 1) {
+                    // if its last point consider round trip distance
+                    maxDistanceBetweenPoints = Math.max(maxDistanceBetweenPoints, 2 * (pointsList.get(i) - pointsList.get(i - 1)));
+                } else {
+                    maxDistanceBetweenPoints = Math.max(maxDistanceBetweenPoints, pointsList.get(i) - pointsList.get(i - 1));
+                }
+
+            }
+            System.out.println(maxDistanceBetweenPoints); // Print the maximum distance
+        }
+        sc.close(); // Close the scanner to prevent resource leaks
+    }
+}
