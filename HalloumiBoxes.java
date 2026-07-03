7893779be193b227663e71c58433dc36ea6eb34e
Kshitiz Rawat <rwtchinu007@gmail.com>
2026-07-03 13:38:29 +0530

    1901A and 1903A

--- HalloumiBoxes.java
+++ HalloumiBoxes.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 public class HalloumiBoxes {
+    //1903A
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();  //read number of test cases
--- LineTrip.java
+++ LineTrip.java
@@ -2,7 +2,7 @@
 import java.util.*;
 
 public class LineTrip {
-
+//1901A
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt(); // Read the number of test cases
